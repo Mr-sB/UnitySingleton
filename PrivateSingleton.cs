2c0f84868f035dd9e7935fed56422350ebf607be
张杰 <jie_zhang@pinssible.com>
2020-03-12 20:07:48 +0800

    update note

--- PrivateSingleton.cs
+++ PrivateSingleton.cs
@@ -3,9 +3,11 @@
 /// </summary>
 public class PrivateSingleton<T> where T : PrivateSingleton<T>
 {
+    //Lazy
     public static T Instance => Nested.Instance;
     private static class Nested
     {
+        //Thread safe
         internal static readonly T Instance = System.Activator.CreateInstance(typeof(T), true) as T;
     }
 }
--- PublicSingleton.cs
+++ PublicSingleton.cs
@@ -3,9 +3,11 @@
 /// </summary>
 public class PublicSingleton<T> where T : PublicSingleton<T>, new()
 {
+    //Lazy
     public static T Instance => Nested.Instance;
     private static class Nested
     {
+        //Thread safe
         internal static readonly T Instance = new T();
     }
 }
